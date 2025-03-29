84ac16b82e30f1009734c5b6bdaabd84cbcca8a5
Estefanie Gisell Peña Batista <estefanie.pena.b@gmail.com>
2025-03-28 19:43:18 -0500

    Modifying last editions of desktop version and menu list

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const menuHamburguer = document.querySelector(".menu-hamburguer");
 const menuList = document.querySelector(".menu-list");
 
+menuList.classList.add("closed");
+
 menuHamburguer.addEventListener("click", toggleMenu);
 
 function toggleMenu (){
