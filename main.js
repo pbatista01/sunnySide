5f7b9c3da242562460e816b0b543faf8b1302069
Estefanie Gisell Peña Batista <estefanie.pena.b@gmail.com>
2025-04-01 12:09:58 -0500

    Adding comments to each file

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+
+// Select the hamburger menu button
 const menuHamburguer = document.querySelector(".menu-hamburguer");
+
+// Select the menu list
 const menuList = document.querySelector(".menu-list");
 
+// Initially hide the menu by adding the "closed" class
 menuList.classList.add("closed");
 
-menuHamburguer.addEventListener("click", toggleMenu);
-
-function toggleMenu (){
-    menuList.classList.toggle("closed");
-}
+// Add a click event listener to the hamburger menu button
+menuHamburguer.addEventListener("click", () => menuList.classList.toggle("closed"));
